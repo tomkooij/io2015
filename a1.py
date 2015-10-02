012c11ad254dd62fc7235258e9911ef9460557bf
tomkooij <tomkooij@tomkooij.nl>
2015-10-02 16:23:09 +0200

    Opgave A1

---
+++ a1.py
@@ -0,0 +1,30 @@
+# Opgave A1
+from __future__ import division
+from __future__ import print_function
+
+TEST = 1
+
+def print_halter(number_of_lines):
+    halter = []
+
+    # generate and print the top part of the "halter"
+    for linenumber in range(int(number_of_lines)):
+        # eerste halter
+        if linenumber < (number_of_lines / 2):
+            number_of_stars = int(number_of_lines - 2*linenumber)
+            line = linenumber * '_'+number_of_stars * '*'+linenumber * '_'
+            print(line)
+            if number_of_stars > 1:
+                halter.append(line)
+
+    # pop the lines in reverse order to print the bottom part "halter"
+    [print(halter.pop()) for _ in range(len(halter))]
+
+if __name__ == '__main__':
+    if TEST:
+        for N in range(1,42):
+            print("N = ", N)
+            print_halter(2*N-1)
+    else:
+        N = int(input("N="))
+        print_halter(2*N-1)
