f4fab4b3b34ada6f41f1f0df90788b6a43239696
tomkooij <tomkooij@tomkooij.nl>
2015-10-03 19:47:37 +0200

    opgave a3

---
+++ a3.py
@@ -0,0 +1,38 @@
+# a3.py
+import sys
+
+TEST = 0
+
+# Uit het voorschrift van de olympiade
+def io_print(string):
+    sys.stdout.write(str(string) + "\n")
+    sys.stdout.flush()
+
+def read_list():
+
+    lijst = []
+
+    while 1:
+        getal = int(raw_input())
+        if getal:
+            lijst.append(getal)
+        else:
+            break
+    return lijst
+
+if __name__ == '__main__':
+
+    if TEST:
+        getallen = [12,14,77,17,7]
+    else:
+        getallen = read_list()
+
+    counter = 0
+
+    for getal in getallen:
+        if (getal % 7 == 0) or (str(getal).find('7') != -1):
+            if TEST:
+                print "DEBUG: gevonden:" , getal
+            counter += 1
+
+    print counter
