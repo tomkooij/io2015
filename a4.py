0c121818ac77edf258fa623855a7d58691e167e1
tomkooij <tomkooij@tomkooij.nl>
2015-10-05 16:44:42 +0200

    Opgave A4 en A5

---
+++ a4.py
@@ -0,0 +1,53 @@
+# Opgave A4
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+TEST = 0
+DEBUG = 0
+
+
+# Uit het voorschrift van de olympiade
+def io_print(string):
+    sys.stdout.write(str(string) + "\n")
+    sys.stdout.flush()
+
+def selecteer_foto(lijst):
+
+    if DEBUG:
+        io_print(lijst)
+
+    eerste_10 = lijst[0:10]
+
+    grootste = max(eerste_10)
+    io_print(grootste)
+
+    plaats_grootste = eerste_10.index(grootste)
+
+    lijst = lijst[plaats_grootste+1:]
+    if DEBUG:
+        io_print(lijst)
+
+    return lijst
+
+
+def selecteer_fotos(stapel):
+
+    while (len(stapel) > 0):
+        stapel = selecteer_foto(stapel)
+        if len(stapel) < 10:
+                break
+
+if __name__ == '__main__':
+    if TEST:
+        N = 22
+        stapel = [9, 12, 17, 22, 8, 1, 20, 3, 15, 21, 2, 19, 4,
+                    18, 5, 16, 14, 13, 11, 7, 6, 10]
+        assert(len(stapel)==N)
+        selecteer_fotos(stapel)
+
+    else:
+        N = int(raw_input(""))
+        lijst = [int(raw_input("")) for _ in range(N)]
+        selecteer_fotos(lijst)
---
+++ a5.py
@@ -0,0 +1,86 @@
+# Opgave A4
+from __future__ import division
+from __future__ import print_function
+
+import sys
+
+TEST = 0
+DEBUG = 0
+
+
+# Uit het voorschrift van de olympiade
+def io_print(string):
+    sys.stdout.write(str(string) + "\n")
+    sys.stdout.flush()
+
+
+def vind_volgende(bord):
+    for (rij, regel) in enumerate(bord):
+        for (kolom, karakter) in enumerate(regel):
+            if karakter == '0':
+                return rij, kolom
+    # niet gevonden
+    return (-1,-1)
+
+
+def kleur_vlak_in(rij, kolom, bord):
+
+    bord = kleur_in(rij,kolom, bord)
+
+    # boven
+    if (rij > 0) and (bord[rij-1][kolom] == '0'):
+        bord = kleur_vlak_in(rij-1, kolom, bord)
+    # onder
+    if (rij < 9) and (bord[rij+1][kolom] == '0'):
+        bord = kleur_vlak_in(rij+1, kolom, bord)
+    # links
+    if (kolom > 0) and (bord[rij][kolom-1] == '0'):
+        bord = kleur_vlak_in(rij, kolom-1, bord)
+    # onder
+    if (kolom < 9) and (bord[rij][kolom+1] == '0'):
+        bord = kleur_vlak_in(rij, kolom+1, bord)
+
+    return bord
+
+
+def kleur_in(rij, kolom, bord):
+    # bord[rij][kolom] = '1' DOES NOT WORK! Strings are immutable
+    bord[rij] = bord[rij][0:kolom]+'1'+bord[rij][kolom+1:]
+    return bord
+
+
+if __name__ == '__main__':
+    if TEST:
+        bord = ['0111000101',
+                '0101000110',
+                '1001111100',
+                '0010000001',
+                '0101010101',
+                '1100010011',
+                '0000000000',
+                '0101010101',
+                '1010101010',
+                '0010010010']
+    else:
+        bord = [int(raw_input("")) for _ in range(10)]
+
+    if DEBUG:
+        print(bord)
+
+    rij = 0
+    kolom = 0
+    count = 0
+
+    while (1):
+        rij, kolom = vind_volgende(bord)
+        if DEBUG:
+            print("gevonden: ", rij, kolom)
+        if (rij == -1):
+            break
+        bord = kleur_vlak_in(rij, kolom, bord)
+        count += 1
+
+    if DEBUG:
+        print(bord)
+
+    print(count)
