fc7158211e7822ef69159f8641e68c538791edc0
tomkooij <tomkooij@tomkooij.nl>
2015-10-11 22:18:26 +0200

    findall -> oplossing nu 54

--- b4.py
+++ b4.py
@@ -1,11 +1,32 @@
 #b4
 # woorden leggen
+#
+# TODO: Handle EERSTE met met 3x 'E'
+# maak lijst met alle oplossingen voor 60 en 56 en controleer juistheid
+
+"""
+NIET JUIST!
+min =  56
+['.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.']
+['.', '.', '.', '.', '.', '.', '.', 'B', '.', '.', '.', '.', '.', '.', '.']
+['.', '.', '.', '.', '.', 'W', '.', 'O', '.', '.', '.', '.', '.', '.', '.']
+['.', '.', '.', '.', '.', 'E', 'E', 'R', 'S', 'T', 'E', '.', '.', '.', '.']
+['.', '.', '.', '.', '.', 'I', '.', 'D', '.', '.', '.', 'R', '.', '.', '.']
+['.', '.', '.', '.', '.', 'G', '.', 'E', '.', 'N', 'I', 'O', '.', '.', '.']
+['.', '.', '.', '.', '.', 'E', '.', 'R', '.', '.', '.', 'N', '.', '.', '.']
+['.', '.', '.', '.', '.', 'R', 'U', 'S', 'L', 'A', 'N', 'D', '.', '.', '.']
+['.', '.', '.', '.', '.', 'T', '.', '.', '.', '.', '.', 'E', '.', '.', '.']
+"""
 
 from copy import deepcopy
 from random import shuffle
 from itertools import permutations
+from re import finditer
 
-woorden = ['DELFT', 'EINDHOVEN', 'LEIDEN', 'NYMEGEN', 'TWENTE']
+test2 = ['EEN', 'TWEE', 'DRIEEE', 'EERSTE']
+opgave = ['NIO', 'EERSTE', 'RONDE','RUSLAND', 'BORDER', 'WEIGERT']
+
+TEST = 0
 
 HORIZONTAAL = 0
 VERTICAAL = 1
@@ -74,6 +95,19 @@ def leg_woord(bord, x, y, orientatie, woord):
 
     return True
 
+def findall_iter(sub, string):
+    """
+    >>> text = "Allowed Hello Hollow"
+    >>> tuple(findall_iter('ll', text))
+    (1, 10, 16)
+    """
+    def next_index(length):
+        index = 0 - length
+        while True:
+            index = string.find(sub, index + length)
+            yield index
+    return iter(next_index(len(sub)).next, -1)
+
 
 def do(bord, woordenlijst):
 
@@ -89,9 +123,8 @@ def do(bord, woordenlijst):
             if karakter != '.':
                 #print "optie", x, y, karakter
                 # HIER 2 of 3 opties toestaan
-                offset = woord.find(karakter)
-                if offset != -1:
-                    # gevonden, probeer te leggen:
+                for offset in findall_iter(karakter, woord):
+
                     tmp_bord = deepcopy(hetbord) # maak kopie!
                     if leg_woord(tmp_bord, x-offset,y, HORIZONTAAL, woord):
                         #print "HORIZONTAAL! :",x,y,offset
@@ -101,6 +134,8 @@ def do(bord, woordenlijst):
                         else:
                             area = bord_area(tmp_bord)
                             if area <= RECORD:
+                                if area == RECORD:
+                                    print_bord(tmp_bord)
                                 RECORD = area
                                 print "RECORD! ", area
                                 RECORDBORD = deepcopy(tmp_bord)
@@ -116,6 +151,8 @@ def do(bord, woordenlijst):
                         else:
                             area = bord_area(tmp_bord)
                             if area <= RECORD:
+                                if area == RECORD:
+                                    print_bord(tmp_bord)
                                 RECORD = area
                                 print "RECORD! ", area
                                 RECORDBORD = deepcopy(tmp_bord)
@@ -134,16 +171,21 @@ def leg_voorbeeld(bord):
     return bord
 
 if __name__ == '__main__':
-
-    #leeg_bord = [['.' for x in range(MAX_X_BORD)] for y in range(MAX_Y_BORD)]
-    #volbord = leg_voorbeeld(leeg_bord)
-    #print_bord(volbord)
-    #print bord_area(volbord)
-    #rest = ['DELFT','EINDHOVEN','NYMEGEN','TWENTE']
-    for case in permutations(woorden):
+    if TEST:
         leeg_bord = [['.' for x in range(MAX_X_BORD)] for y in range(MAX_Y_BORD)]
-        print case
-        leg_woord(leeg_bord,5,2,VERTICAAL,case[0])
-        do(leeg_bord, case[1:])
-    print "min = ", RECORD
-    print_bord(RECORDBORD)
+        volbord = leg_voorbeeld(leeg_bord)
+        print_bord(volbord)
+        print bord_area(volbord)
+        rest = ['DELFT','EINDHOVEN','NYMEGEN','TWENTE']
+        for case in permutations(rest):
+            bord = deepcopy(volbord)
+            print case
+            do(leeg_bord, case)
+    else:
+        for case in permutations(opgave):
+            leeg_bord = [['.' for x in range(MAX_X_BORD)] for y in range(MAX_Y_BORD)]
+            print case
+            leg_woord(leeg_bord,5,2,VERTICAAL,case[0])
+            do(leeg_bord, case[1:])
+        print "min = ", RECORD
+        print_bord(RECORDBORD)
