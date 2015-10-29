fdc72906119e473863a26fab6dfa9fae198814ff
tomkooij <tomkooij@tomkooij.nl>
2015-10-29 16:15:22 +0100

    stderr debug output

--- board.py
+++ board.py
@@ -1,5 +1,6 @@
 from math import copysign
-from random import choice
+import random
+import sys
 
 sign = lambda x: int(copysign(1, x))
 
@@ -35,9 +36,9 @@ class Bord(object):
         for i in range(N):
             for j in range(N):
                 if self.bord[i][j]:
-                    print '%4d' % copysign(3**(abs(self.bord[i][j])-1),self.bord[i][j]),
+                    print >>sys.stderr, '%4d' % copysign(3**(abs(self.bord[i][j])-1),self.bord[i][j]),
                 else:
-                    print '   .',
+                    print >>sys.stderr, '   .',
             print
 
     def rotate(self, number_of_rotations):    # roteer links om
@@ -67,7 +68,7 @@ class Bord(object):
             self.rotate(3)
 
         else:
-            print "illegal move: ", direction
+            print >>sys.stderr, "illegal move: ", direction
             assert(0)
 
     def place(self, row, column, kleur):
@@ -137,7 +138,7 @@ class Bord(object):
         """
         places a random tile, return position in game format ie: "24" -> [1][3]
         """
-        place = choice(self._free_squares())
+        place = random.choice(self._free_squares())
 
         row = place[0]
         column = place[1]
@@ -162,7 +163,7 @@ class Bord(object):
         elif move == 'L':
             b.move(LEFT)
         else:
-            print "illegal move!"
+            print >>sys.stderr, "illegal move!"
             assert(0)
 
 if __name__ == '__main__':
@@ -173,13 +174,13 @@ if __name__ == '__main__':
     MOVES=['12', '33', '22', '11', 'R', 'L', '22', '41', '42', '12', 'U', 'R', '31', '33', '11', '41', 'R', 'D', '13', '21', '41', '32', 'U', 'D', '21', '23', '13', '22', 'U', 'D', '12', '23', '13', '22', 'R', 'U', '11', '44', '33', '43', 'D', 'R', '12', '41', '32', '13', 'R', 'L', '24', '34', '14', '23', 'R', 'U', '44', '32', '41', '33', 'U', 'D', '14', '31', '33', '13', 'R', 'U', '41', '23', '44', '31', 'U', 'R', '43', '32', '23', '41', 'R', 'L', '24', '33', '43', '42', 'U', 'L', '34', '43', '24', '32', 'R', 'R', '41', '43', '32', '31', 'D', 'U', '21', '34', '41', '31', 'R', 'L', '34', '42', '33', '32', 'U', 'R', '42', '31', '11', '21', 'D', 'U', '31', '44', '21', '41', 'U', 'L', '33', '42', '34', '23', 'L', 'R', '42', '43', '21', '32', 'L', 'R', '43', '41', '31', '42', 'U', 'D', '12', '14', '21', '22', 'L', 'U', '14', '43', '34', '24', 'L', 'D', '24', '11', '13', '34', 'R', 'L', '13', '14', '24', '34', 'D', 'U', '42', '34', '44', '33', 'R', 'U', '43', '21', '41', '44', 'R', 'L', '23', '33', '44', '13', 'D', 'U', '33', '24', '34', '43', 'L', 'R', '12', '21', '11', '32', 'U', 'D', '11', '42', '32', '13', 'U', 'L', '33', '24', '34', '44', 'R', 'L', '33', '42', '34', '43', 'U', 'R', '31', '43', '42', '41', 'R', 'U', '42', '41'];
 
     b = Bord()
-    print "Start!"
+    print >>sys.stderr, "Start!"
 
     zetteller = 0  # hulpje om blauw, rood, rood, blauw t regelen
     aanzet = [BLUE, RED, RED, BLUE, None, None]
 
     for i,move in enumerate(MOVES):
-        print "ronde (%d) %s" % (i+1, move)
+        print >>sys.stderr, "ronde (%d) %s" % (i+1, move)
         b.move_game_notation(move, aanzet[zetteller])
 
         # zorg dat de juiste speler aan zet is
