7af55e2842926c847d42bb83b7d8fc87c7b0d7dd
tomkooij <tomkooij@tomkooij.nl>
2015-10-29 15:40:17 +0100

    move_game_notation() implemented

--- board.py
+++ board.py
@@ -146,6 +146,24 @@ class Bord(object):
 
         return '%d%d' % (row+1,column+1)
 
+    def move_game_notation(self, move, color=RED):
+        """
+        verwerk een zet in "game notation"
+        move => str '23' of 'U'
+        """
+        if len(move)==2:
+            b.place(int(move[0])-1, int(move[1])-1, color)
+        elif move == 'U':
+            b.move(UP)
+        elif move == 'D':
+            b.move(DOWN)
+        elif move == 'R':
+            b.move(RIGHT)
+        elif move == 'L':
+            b.move(LEFT)
+        else:
+            print "illegal move!"
+            assert(0)
 
 if __name__ == '__main__':
     """
@@ -155,30 +173,19 @@ if __name__ == '__main__':
     MOVES=['12', '33', '22', '11', 'R', 'L', '22', '41', '42', '12', 'U', 'R', '31', '33', '11', '41', 'R', 'D', '13', '21', '41', '32', 'U', 'D', '21', '23', '13', '22', 'U', 'D', '12', '23', '13', '22', 'R', 'U', '11', '44', '33', '43', 'D', 'R', '12', '41', '32', '13', 'R', 'L', '24', '34', '14', '23', 'R', 'U', '44', '32', '41', '33', 'U', 'D', '14', '31', '33', '13', 'R', 'U', '41', '23', '44', '31', 'U', 'R', '43', '32', '23', '41', 'R', 'L', '24', '33', '43', '42', 'U', 'L', '34', '43', '24', '32', 'R', 'R', '41', '43', '32', '31', 'D', 'U', '21', '34', '41', '31', 'R', 'L', '34', '42', '33', '32', 'U', 'R', '42', '31', '11', '21', 'D', 'U', '31', '44', '21', '41', 'U', 'L', '33', '42', '34', '23', 'L', 'R', '42', '43', '21', '32', 'L', 'R', '43', '41', '31', '42', 'U', 'D', '12', '14', '21', '22', 'L', 'U', '14', '43', '34', '24', 'L', 'D', '24', '11', '13', '34', 'R', 'L', '13', '14', '24', '34', 'D', 'U', '42', '34', '44', '33', 'R', 'U', '43', '21', '41', '44', 'R', 'L', '23', '33', '44', '13', 'D', 'U', '33', '24', '34', '43', 'L', 'R', '12', '21', '11', '32', 'U', 'D', '11', '42', '32', '13', 'U', 'L', '33', '24', '34', '44', 'R', 'L', '33', '42', '34', '43', 'U', 'R', '31', '43', '42', '41', 'R', 'U', '42', '41'];
 
     b = Bord()
-    b.show()
     print "Start!"
 
     zetteller = 0  # hulpje om blauw, rood, rood, blauw t regelen
-    aanzet = [BLUE, RED, RED, BLUE]
+    aanzet = [BLUE, RED, RED, BLUE, None, None]
 
-    for move in MOVES:
-        print "Move : ", move
+    for i,move in enumerate(MOVES):
+        print "ronde (%d) %s" % (i+1, move)
+        b.move_game_notation(move, aanzet[zetteller])
+
+        # zorg dat de juiste speler aan zet is
         if move in ['U', 'D', 'R', 'L']:
-            if move == 'U':
-                b.move(UP)
-            elif move == 'D':
-                b.move(DOWN)
-            elif move == 'R':
-                b.move(RIGHT)
-            elif move == 'L':
-                b.move(LEFT)
-            else:
-                assert(0)
-            zetteller = 0
+            zetteller = 0   # na een shif opnieuw beginnen
         else:
-
-            print "place ", aanzet[zetteller]
-            b.place(int(move[0])-1, int(move[1])-1, aanzet[zetteller])
             zetteller += 1 # de ander is nu aan zet
         b.show()
         raw_input()
